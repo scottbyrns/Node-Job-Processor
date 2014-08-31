7b709cd693e51caa5e4217338a0f616cf4b89046
Scott Byrns <contact@scottbyrns.com>
2014-08-31 00:34:51 -0600

    Stub for document scraping.

--- JobHandlers/SATELLITE-DATA-READY-FOR-PROCESSING.js
+++ JobHandlers/SATELLITE-DATA-READY-FOR-PROCESSING.js
@@ -1,3 +1,34 @@
 var tools = require('./JobTools');
 
+var cheerio = require('cheerio'),
+var fs = require('fs')
+
+fs.readFile('satelliteList.php.raw', 'utf8', function (err, fileContents) {
+	
+	if (err) {
+	
+		return console.log(err);
+	
+	}
+	
+	$ = cheerio.load(fileContents);
+	
+	var rows = $("#main tbody tr").each(function(i, elem) {
+		
+		$(this).find("td").each(function (i, elem) {
+			
+			console.log($(this).text());
+			
+		});
+		
+		// fruits[i] = $(this).text();
+	});;
+	
+});
+
+
+
+
+
+
 tools.shutdown();

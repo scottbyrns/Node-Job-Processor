3ebd2444ab27e24264153b310622e79f616b93f3
Scott Byrns <contact@scottbyrns.com>
2014-08-31 21:18:40 -0600

    Adding NEXRAD site data persistence job.

--- JobHandlers/UPDATE-CBX-N0R-0.js
+++ JobHandlers/UPDATE-CBX-N0R-0.js
@@ -4,7 +4,7 @@ var path = "http://radar.weather.gov/ridge/RadarImg/N0R/CBX_N0R_0.gif";
 
 tools.download(path, "CBX-N0R-0.gif", function (filename) {
 
-	tools.emit("EVENT", "CBX-N0R-0-DID-UPDATE");		
+	tools.emit("EVENT", "CBX-N0R-0-DID-UPDATE");
 	tools.shutdown();
 
 });

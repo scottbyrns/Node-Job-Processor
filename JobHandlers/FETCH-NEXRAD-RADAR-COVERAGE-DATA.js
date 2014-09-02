b3a6b27368f36a0127ffbbb841235c34bc6dfeaa
Scott Byrns <contact@scottbyrns.com>
2014-09-01 21:27:46 -0600

    Bad URL catch to ensure complete run.

--- JobHandlers/FETCH-NEXRAD-RADAR-COVERAGE-DATA.js
+++ JobHandlers/FETCH-NEXRAD-RADAR-COVERAGE-DATA.js
@@ -29,31 +29,41 @@ tools.client.llen("NEXRAD-SITE", function (err, len) {
 				var lindex = index;
 				var place = site;
 				return function(error, response, body) {
-					var blob = body.split('<script language="JavaScript" type="text/javascript">')[1].split("</script>")[0];
-					var Mi = blob.split("Mi = ")[1].split(";")[0]                ;
-					var Lat = blob.split("Lat = ")[1].split(";")[0]              ;
-					var xyperpixel = blob.split("xyperpixel = ")[1].split(";")[0];
-					var xllcorner = blob.split("xllcorner = ")[1].split(";")[0]  ;
-					var yllcorner = blob.split("yllcorner = ")[1].split(";")[0]  ;
+					
+					try {
+						var blob = body.split('<script language="JavaScript" type="text/javascript">')[1].split("</script>")[0];
+						var Mi = blob.split("Mi = ")[1].split(";")[0]                ;
+						var Lat = blob.split("Lat = ")[1].split(";")[0]              ;
+						var xyperpixel = blob.split("xyperpixel = ")[1].split(";")[0];
+						var xllcorner = blob.split("xllcorner = ")[1].split(";")[0]  ;
+						var yllcorner = blob.split("yllcorner = ")[1].split(";")[0]  ;
 				
-					site.coverage = {
-						mi: Mi,
-						lat: Lat,
-						xyperpixel: xyperpixel,
-						xllcorner: xllcorner,
-						yllcorner: yllcorner
-					};
+						site.coverage = {
+							mi: Mi,
+							lat: Lat,
+							xyperpixel: xyperpixel,
+							xllcorner: xllcorner,
+							yllcorner: yllcorner
+						};
 				
-					tools.client.lset("NEXRAD-SITE", lindex, JSON.stringify(place), function () {
+						tools.client.lset("NEXRAD-SITE", lindex, JSON.stringify(place), function () {
+							counter -= 1;
+							if (counter == 0) {
+								tools.emit("EVENT", "NEXRAD-SITES-COVERAGE-WAS-FETCHED");
+								tools.shutdown();
+							}
+						});
+				
+				
+					}
+					catch (e) {
 						counter -= 1;
+						console.error(site.site, lindex, e);
 						if (counter == 0) {
 							tools.emit("EVENT", "NEXRAD-SITES-COVERAGE-WAS-FETCHED");
 							tools.shutdown();
 						}
-					});
-				
-				
-					
+					}
 				}
 			}(i, site));
 

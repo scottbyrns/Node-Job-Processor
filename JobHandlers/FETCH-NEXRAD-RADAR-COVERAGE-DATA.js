bedfb01a218b065c24081fbb70d1962cb6c68be3
Scott Byrns <contact@scottbyrns.com>
2014-09-01 21:21:12 -0600

    Fixing indexing issue, adding shutdown counter.

--- JobHandlers/FETCH-NEXRAD-RADAR-COVERAGE-DATA.js
+++ JobHandlers/FETCH-NEXRAD-RADAR-COVERAGE-DATA.js
@@ -9,9 +9,9 @@ tools.client.llen("NEXRAD-SITE", function (err, len) {
 
 	var counter = len;
 
-	for (var i = 0; i < 2; i += 1) {
+	for (var i = 0; i < len; i += 1) {
 	
-		tools.client.lindex("NEXRAD-SITE", i, function (err, result) {
+		tools.client.lindex("NEXRAD-SITE", i, function (i) { return function (err, result) {
 	
 			if (err) {
 				return console.error(err);
@@ -22,31 +22,42 @@ tools.client.llen("NEXRAD-SITE", function (err, len) {
 		
 			var url = "http://radar.weather.gov/ridge/radar.php?rid=" + siteKey + "&product=N0R&overlay=01000000&loop=no";
 		
-			var index = i*1;
- 
+
 			request({
 			  uri: url,
-			}, function(error, response, body) {
-				var blob = body.split('<script language="JavaScript" type="text/javascript">')[1].split("</script>")[0];
-				var Mi = blob.split("Mi = ")[1].split(";")[0]                ;
-				var Lat = blob.split("Lat = ")[1].split(";")[0]              ;
-				var xyperpixel = blob.split("xyperpixel = ")[1].split(";")[0];
-				var xllcorner = blob.split("xllcorner = ")[1].split(";")[0]  ;
-				var yllcorner = blob.split("yllcorner = ")[1].split(";")[0]  ;
+			}, function (index, site) {
+				var lindex = index;
+				var place = site;
+				return function(error, response, body) {
+					var blob = body.split('<script language="JavaScript" type="text/javascript">')[1].split("</script>")[0];
+					var Mi = blob.split("Mi = ")[1].split(";")[0]                ;
+					var Lat = blob.split("Lat = ")[1].split(";")[0]              ;
+					var xyperpixel = blob.split("xyperpixel = ")[1].split(";")[0];
+					var xllcorner = blob.split("xllcorner = ")[1].split(";")[0]  ;
+					var yllcorner = blob.split("yllcorner = ")[1].split(";")[0]  ;
 				
-				site.coverage = {
-					mi: Mi,
-					lat: Lat,
-					xyperpixel: xyperpixel,
-					xllcorner: xllcorner,
-					yllcorner: yllcorner
-				};
+					site.coverage = {
+						mi: Mi,
+						lat: Lat,
+						xyperpixel: xyperpixel,
+						xllcorner: xllcorner,
+						yllcorner: yllcorner
+					};
 				
-				tools.client.lset("NEXRAD-SITE", index, JSON.stringify(site));
+					tools.client.lset("NEXRAD-SITE", lindex, JSON.stringify(place), function () {
+						counter -= 1;
+						if (counter == 0) {
+							tools.emit("EVENT", "NEXRAD-SITES-COVERAGE-WAS-FETCHED");
+							tools.shutdown();
+						}
+					});
 				
-			});
+				
+					
+				}
+			}(i, site));
 
-		});
+		}}(i));
 	
 	}
 	
